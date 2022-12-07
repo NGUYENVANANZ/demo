0f482bce962e789a0f71ca26082a88c1c952701e
manhhoang98 <110440251+manhhoang98@users.noreply.github.com>
2022-12-07 11:00:01 +0700

    manh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
+
     public static void main(String[] args) {
+        String name = "manh";
+        System.out.println(name);
         System.out.println("Hello world!");
     }
 }
