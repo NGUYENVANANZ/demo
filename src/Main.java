99aae765907a1df0c9e9a41fecc57d526101c46f
Linh3009 <109840475+Linh3009@users.noreply.github.com>
2022-12-07 10:58:52 +0700

    up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        String linh = "linh";
+        System.out.println(linh);
         System.out.println("Hello world!");
     }
 }
